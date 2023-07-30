14eb9c024737f876189bd90d726d584068fdbb22
AvivR <avivrab@wix.com>
2023-07-30 14:25:47 +0300

    current daata

--- mentalStrategiesProject.py
+++ mentalStrategiesProject.py
@@ -4,6 +4,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 import statsmodels.api as sm
+import sklearn as sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.feature_selection import RFE
@@ -12,6 +13,7 @@ from sklearn.feature_selection import VarianceThreshold
 from sklearn.datasets import load_iris
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif
+import math
 
 # read data set
 data = pd.read_csv(
@@ -19,15 +21,17 @@ data = pd.read_csv(
 )
 df = pd.DataFrame(data)
 
-
-print("before", df.shape)
-
 ###----01------------------PREPROCESSING DATA---------------------###
+df = df.fillna(0)
+
+###----------------- extract success rates -----------------------###
+success_cols_names = ["Qhat"]
+df_filtered = df.drop(success_cols_names, axis="columns")
 ###----------------- remove low variance columns -----------------###
 variances = df.loc[:, ~df.columns.isin(["sub_num", "session_num"])].var()
 variance_threshold = 0.025
 low_variance_columns = variances[variances < variance_threshold].index
-df_filtered = df.drop(low_variance_columns, axis="columns")
+df_filtered = df_filtered.drop(low_variance_columns, axis="columns")
 df_filtered.to_csv(
     "/Users/avivrab/Documents/CS-workshop/CS-Brain-Research-Workshop/modified_file.csv",
     index=False,
@@ -53,34 +57,34 @@ for i in range(len(correlation_matrix.columns)):
 
 # Remove highly correlated features from the dataset
 df_filtered = df_filtered.drop(highly_correlated, axis="columns")
-correlation_matrix = correlation_matrix.drop(highly_correlated, axis="columns")
+correlation_matrix = correlation_matrix.drop(highly_correlated, axis="columns").drop(
+    highly_correlated, axis="rows"
+)
 # Save filtered dataset to CSV
 df_filtered.to_csv(
     "/Users/avivrab/Documents/CS-workshop/CS-Brain-Research-Workshop/filtered_dataset.csv",
     index=False,
 )
-
 ###------------------FEATURE EXTRACTION FUNCS---------------------###
 
-
-def softCos(a, b, matrix):
+def softCos(table, a, b, matrix):
     # nominator calculation
     sum_nominator = 0
     for i in range(len(a)):
         for j in range(len(b)):
-            sum_nominator += (matrix[i, j]) * (a[i]) * (b[j])
+            sum_nominator += matrix.iloc[i, j] * (a.iloc[i]) * (b.iloc[j])
 
     # denominator nrom values calculation
     sum_norm_a = 0
     for i in range(len(a)):
         for j in range(len(a)):
-            sum_norm_a += matrix[i, j] * (a[i]) * (a[j])
+            sum_norm_a += matrix.iloc[i, j] * (a.iloc[i]) * (a.iloc[j])
     norm_a = np.sqrt(sum_norm_a)
 
     sum_norm_b = 0
     for i in range(len(b)):
         for j in range(len(b)):
-            sum_norm_b += matrix[i, j] * (b[i]) * (b[j])
+            sum_norm_b += matrix.iloc[i, j] * (b.iloc[i]) * (b.iloc[j])
     norm_b = np.sqrt(sum_norm_b)
 
     # calculate softcosine between a and b
@@ -89,37 +93,68 @@ def softCos(a, b, matrix):
     return sftcos
 
 
-def cosineWithinSubject(table, subject_features):
-    rows_arr = []
-    for row in range(len(table) - 1):
-        columns_arr = []
-        for column in range(row + 1, len(table)):
-            columns_arr.append(
-                softCos(session[row], session[column], correlation_matrix)
-            )
-        rows_arr.append(columns_arr)
-    subject_features.append(np.array(rows_arr).mean())
+def cosineWithinSubject(table, subject_features, classify_sessions):
+    if classify_sessions == "within":
+        soft_cosine_arr = []
+    for run_a in range(len(table) - 1):
+        for run_b in range(run_a + 1, len(table)):
+            if classify_sessions == "within":
+                soft_cosine_arr.append(
+                    softCos(table,table.iloc[run_a], table.iloc[run_b], correlation_matrix)
+                )
+            else:  # classify == "between"
+                subject_features.append(
+                    softCos(table,table.iloc[run_a], table.iloc[run_b], correlation_matrix)
+                )
+    if classify_sessions == "within":
+        subject_features.append(np.array(soft_cosine_arr).mean())
 
 
 def meanVectorForEachSession(session):
-    return np.array(session.mean(axis=1))
+    return np.array(session).mean(axis=0)
 
 
 ###----02---PROCESSING WITHIN SUBJECT: FEATURE EXTRACTION---------###
-
+exit()
 per_subject = df_filtered.groupby(["sub_num"])
 subjects = []
-for subject_num, subject in enumerate(per_subject):
+for subject_tuple in per_subject:
+    subject_trial = subject_tuple[1]
+    subject_num = subject_tuple[0][0]
     subject_features = []
-    all_sessions = subject.groupby(["session_num"])
+    subject_sessions = subject_trial.groupby(["session_num"])
+    #subject has less than 5 sessions
+    if len(subject_sessions) < 5:
+        # print(f"subject {subject_num} has {len(subject_sessions)} sessions")
+        continue
     strategies_cosines = []
     mean_sessions = []
     # cosine for session (5 runs)
-    for session_num, session in enumerate(all_sessions):
-        cosineWithinSubject(session, subject_features)  #### 6 features
+    for session_tuple in subject_sessions:
+        session = session_tuple[1].drop(["session_num", "sub_num"], axis="columns")
+        session_num = session_tuple[0][0]
+        # session has less than 3 runs -> don't perform cosine within session
+        if len(session) >= 3:
+            # print(f"subject {subject_num} has {len(session)} runs in session {session_num}")
+            cosineWithinSubject(
+                session, subject_features, classify_sessions="within"
+            )  #### 6 features
         mean_sessions.append(
             meanVectorForEachSession(session)
         )  # mean_sessions is of 6th dim for each run
-    cosineWithinSubject(mean_sessions, subject_features)
-    subjects.append(subject_features)
+    cosineWithinSubject(
+        pd.DataFrame(mean_sessions), subject_features, classify_sessions = "between"
+    )
+    subjects.append((subject_num, subject_features))
+    pd.DataFrame(subjects).to_csv("/Users/avivrab/Documents/CS-workshop/CS-Brain-Research-Workshop/subjects_features.csv",
+    index=False,)
 ###---------------------------------------------------------------###
+
+###-------03---PROCESSING BETWEEN SUBJECT: LINEAR PREDICTION MODEL---------###
+# pca = sklearn.decomposition.PCA(n_components=2)
+# subjects_pca = []
+# for subject_tuple in subjects:
+#     subject_num = subject_tuple[0]
+#     subject_features = subject_tuple[1]
+#     subjects_pca.append(pca.fit_transform(np.array(subject_features))) #pca recieves 2d array
+#     print(f"subject {subject_num} pca: {subjects_pca[subject_num]}")
